82a9428886321d301cf4151a96e3eee590c77b71
Aleksandr Koltovich <avkoltovich@gmail.com>
2020-01-31 02:56:40 +0300

    Исправляет замечания наставника

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@
 
 var NUMBER_OF_ADS = 8;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var TIMES_OF_CHECK_IN = ['12:00', '13:00', '14:00'];
-var TIMES_OF_CHECKOUT = ['12:00', '13:00', '14:00'];
+var PRICES = [10000, 5000, 1000, 0];
+var GUESTS = [1, 2, 3];
+var ROOMS = [1, 2, 3, 100];
+var CHECKIN_TIME = ['12:00', '13:00', '14:00'];
+var CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var LOCATION_MIN_Y = 130;
@@ -18,12 +21,8 @@ var getRandomInteger = function (min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
 
-var getRandomArrayIndex = function (array) {
-  return getRandomInteger(0, array.length - 1);
-};
-
 var getRandomArrayItem = function (array) {
-  return array[getRandomArrayIndex(array)];
+  return array[getRandomInteger(0, array.length - 1)];
 };
 
 var getMixedArray = function (array) {
@@ -42,54 +41,40 @@ var getMixedArray = function (array) {
 };
 
 var getClippedArray = function (array) {
-  var clippedArray = [];
-
-  for (var i = 0; i < getRandomArrayIndex(array); i++) {
-    clippedArray.push(array[i]);
-  }
-
-  return clippedArray;
+  return array.slice(0, getRandomInteger(1, array.length));
 };
 
-var generateArrayOfAvatars = function (number) {
-  var ArrayOfAvatar = [];
-
-  for (var i = 0; i < number; i++) {
-    ArrayOfAvatar[i] = 'img/avatars/user0' + (i + 1) + '.png';
-  }
-
-  return ArrayOfAvatar;
-};
-
-var generateRandomAd = function (avatars) {
+var generateRandomAd = function (index) {
   var ad = {author: {}, offer: {}, location: {}};
-  ad.author.avatar = avatars.pop();
-  ad.offer.title = 'Заголовок предложения';
-  ad.offer.address = '600, 350';
-  ad.offer.price = 0;
-  ad.offer.type = getRandomArrayItem(TYPES);
-  ad.offer.rooms = 0;
-  ad.offer.guests = 0;
-  ad.offer.checkin = getRandomArrayItem(TIMES_OF_CHECK_IN);
-  ad.offer.checkout = getRandomArrayItem(TIMES_OF_CHECKOUT);
-  ad.offer.features = getClippedArray(getMixedArray(FEATURES));
-  ad.offer.description = '';
-  ad.offer.photos = getClippedArray(getMixedArray(PHOTOS));
+
+  ad.author.avatar = 'img/avatars/user0' + (index + 1) + '.png';
+
   ad.location.x = getRandomInteger(LOCATION_MIN_X, locationMaxX);
   ad.location.y = getRandomInteger(LOCATION_MIN_Y, LOCATION_MAX_Y);
+
+  ad.offer.title = 'Здесь будет заголовок предложения';
+  ad.offer.address = ad.location.x + ', ' + ad.location.y;
+  ad.offer.priceS = getRandomArrayItem(PRICES);
+  ad.offer.type = getRandomArrayItem(TYPES);
+  ad.offer.rooms = getRandomArrayItem(ROOMS);
+  ad.offer.guests = getRandomArrayItem(GUESTS);
+  ad.offer.checkin = getRandomArrayItem(CHECKIN_TIME);
+  ad.offer.checkout = getRandomArrayItem(CHECKOUT_TIME);
+  ad.offer.features = getClippedArray(getMixedArray(FEATURES));
+  ad.offer.description = 'Здесь будет описание';
+  ad.offer.photos = getClippedArray(getMixedArray(PHOTOS));
 
   return ad;
 };
 
-var createArrayOfAds = function (number) {
-  var ArrayOfAds = [];
-  var arrayOfAvatars = getMixedArray(generateArrayOfAvatars(number));
+var createAds = function (number) {
+  var Ads = [];
 
   for (var i = 0; i < number; i++) {
-    ArrayOfAds[i] = generateRandomAd(arrayOfAvatars);
+    Ads[i] = generateRandomAd(i);
   }
 
-  return ArrayOfAds;
+  return Ads;
 };
 
 var renderAdPin = function (ad) {
@@ -113,5 +98,5 @@ var createPinsBlock = function (ads) {
   return fragment;
 };
 
-pinsBlock.appendChild(createPinsBlock(createArrayOfAds(NUMBER_OF_ADS)));
+pinsBlock.appendChild(createPinsBlock(createAds(NUMBER_OF_ADS)));
 map.classList.remove('map--faded');
